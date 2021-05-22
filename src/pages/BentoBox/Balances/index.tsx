0db67ce0317ad34f896765be9ea9904b1f5985e8
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-05-22 08:43:18 -0400

    Update index.tsx

--- src/pages/BentoBox/Balances/index.tsx
+++ src/pages/BentoBox/Balances/index.tsx
@@ -40,9 +40,9 @@ export default function BentoBalances(): JSX.Element {
                     <Card
                         className="h-full bg-dark-900"
                         backgroundImage={BentoBoxImage}
-                        title={i18n._(t`Deposit tokens into BentoBox for all the yields`)}
+                        title={i18n._(t`Deposit tokens into the BigVault for all the yields`)}
                         description={i18n._(
-                            t`BentoBox provides extra yield on deposits with flash lending, strategies, and fixed, low-gas transfers among integrated dapps, like Kashi markets`
+                            t`BigVault provides extra yield on deposits with flash lending, strategies, and fixed, low-gas transfers among integrated dapps, like BigBucks markets`
                         )}
                     />
                 }
@@ -53,7 +53,7 @@ export default function BentoBalances(): JSX.Element {
                         <CardHeader className="flex justify-between items-center bg-dark-800">
                             <div className="flex flex-col md:flex-row items-center justify-between w-full">
                                 <div className="flex items-baseline">
-                                    <div className="text-3xl text-high-emphesis mr-4">{i18n._(t`BentoBox`)}</div>
+                                    <div className="text-3xl text-high-emphesis mr-4">{i18n._(t`BigVault`)}</div>
                                 </div>
                                 <div className="flex justify-end w-full py-4 md:py-0">
                                     <Search search={search} term={term} />
@@ -66,7 +66,7 @@ export default function BentoBalances(): JSX.Element {
                         <div className="px-4 grid grid-cols-3 text-sm  text-secondary select-none">
                             <div>{i18n._(t`Token`)}</div>
                             <div className="text-right">{i18n._(t`Wallet`)}</div>
-                            <div className="text-right">{i18n._(t`BentoBox`)}</div>
+                            <div className="text-right">{i18n._(t`BigVault`)}</div>
                         </div>
                         {items &&
                             items.length > 0 &&
